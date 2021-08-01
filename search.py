d50516eb502c23eb0f457015a15337e22cd5adf8
Ben Bauer <ben.bauer1@gmail.com>
2021-08-01 14:59:40 -0500

    sends search to bar and presses enter

--- search.py
+++ search.py
@@ -1,14 +1,16 @@
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 WEBPAGE = "https://en.wikipedia.org/wiki/Main_Page"
 
 driver = webdriver.Chrome("/Users/benbauer/Documents/VSCode/chromedriver")
 
 driver.get(WEBPAGE)
-articles = driver.find_element_by_css_selector("#articlecount a")
 
-articles.click()
+searchbar = driver.find_element_by_name("search")
 
-print(articles.text)
+searchbar.send_keys("Python")
+searchbar.send_keys(Keys.RETURN)
 
 driver.quit()
